4ed94b42cbf75e7ab6a473ea513312bb69241f00
petecky <petecky@95547c93-8e23-0410-a2b5-d5baf7443737>
2009-11-29 09:56:08 +0000

    Repaired I10N

--- trunk/de.berlios.quotations/src/de/berlios/quotations/util/Messages.java
+++ trunk/de.berlios.quotations/src/de/berlios/quotations/util/Messages.java
@@ -4,7 +4,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-    private static final String BUNDLE_NAME = "de.berlios.quotations.util.messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
 
     private static final ResourceBundle RESOURCE_BUNDLE = Utf8ResourceBundle
             .getBundle(BUNDLE_NAME);
